1eb4b532cb672d55c4d5e17d1914f5441f3b0dbf
velezmolina <velezmolina@gmail.com>
2024-12-21 01:11:15 +0000

    Initial commit

---
+++ src/firebase/client.ts
@@ -0,0 +1,16 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:4004561286.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:4190476402.
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAwY0QM8UO9QsYhBKQc9IAeLE7iu4KBKqQ",
+    authDomain: "stlist-4402f.firebaseapp.com",
+    databaseURL: "https://stlist-4402f.firebaseio.com",
+    projectId: "stlist-4402f",
+    storageBucket: "stlist-4402f.appspot.com",
+    messagingSenderId: "955361731943",
+    appId: "1:955361731943:web:c677216bc1e4bc5c767bc2"
+  };
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
