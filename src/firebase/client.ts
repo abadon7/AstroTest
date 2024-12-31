347b6aadba03c87ac71e4c4c11cc5a67ea2b0c79
Henry Vélez <velezmolina@gmail.com>
2024-12-30 23:02:03 -0500

    Sign in  and realtime databse working

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -1,16 +1,39 @@
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:4004561286.
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:4190476402.
+//import { auth } from "firebase-admin";
 import { initializeApp } from "firebase/app";
+import { signInWithPopup, getAuth, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwY0QM8UO9QsYhBKQc9IAeLE7iu4KBKqQ",
-    authDomain: "stlist-4402f.firebaseapp.com",
-    databaseURL: "https://stlist-4402f.firebaseio.com",
-    projectId: "stlist-4402f",
-    storageBucket: "stlist-4402f.appspot.com",
-    messagingSenderId: "955361731943",
-    appId: "1:955361731943:web:c677216bc1e4bc5c767bc2"
-  };
+  apiKey: "AIzaSyAwY0QM8UO9QsYhBKQc9IAeLE7iu4KBKqQ",
+  authDomain: "stlist-4402f.firebaseapp.com",
+  databaseURL: "https://stlist-4402f.firebaseio.com",
+  projectId: "stlist-4402f",
+  storageBucket: "stlist-4402f.appspot.com",
+  messagingSenderId: "955361731943",
+  appId: "1:955361731943:web:c677216bc1e4bc5c767bc2"
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+//const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const userCredential = await signInWithPopup(auth, provider);
+  const idToken = await userCredential.user.getIdToken();
+  console.log(idToken);
+  const res = await fetch("/api/auth/signin", {
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
+  });
+
+  if (res.redirected) {
+    window.location.assign(res.url);
+  }
+};
+
+export { auth, signInWithGoogle, onAuthStateChanged };
--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,12 +1,16 @@
 import type { ServiceAccount } from "firebase-admin";
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import admin from 'firebase-admin';
+import { getApps } from 'firebase-admin/app';
 
-const activeApps = getApps();
+// Configuración del servicio con variables de entorno
+
+const databaseURL = import.meta.env.FIREBASE_DATABASE_URL || "https://stlist-4402f.firebaseio.com";
+const privateKey = import.meta.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
 const serviceAccount = {
   type: "service_account",
   project_id: import.meta.env.FIREBASE_PROJECT_ID,
   private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
+  private_key: privateKey,
   client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
   client_id: import.meta.env.FIREBASE_CLIENT_ID,
   auth_uri: import.meta.env.FIREBASE_AUTH_URI,
@@ -15,16 +19,33 @@ const serviceAccount = {
   client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
 };
 
-const initApp = () => {
-  if (import.meta.env.PROD) {
-    console.info('PROD env detected. Using default service account.')
-    // Use default config in firebase functions. Should be already injected in the server by Firebase.
-    return initializeApp()
+const activeApps = getApps();
+console.log(activeApps.length)
+if (!activeApps.length) {
+  console.log("Initializing Firebase Admin SDK...");
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as ServiceAccount),
+      //databaseURL: import.meta.env.FIREBASE_DATABASE_URL,
+      databaseURL
+    });
+    console.info("Firebase Admin initialized.");
+  } catch (error) {
+    console.error("Error initializing Firebase Admin SDK:", error);
+    throw error;
   }
-  console.info('Loading service account from env.')
-  return initializeApp({
-    credential: cert(serviceAccount as ServiceAccount)
-  })
+} else {
+  console.info("Firebase Admin SDK already initialized.");
 }
-
-export const app = activeApps.length === 0 ? initApp() : activeApps[0];
+// Exporta los servicios necesarios
+const app = activeApps[0] || admin.app();
+//console.log(app)
+const db = admin.database();
+/* const dbs = db;
+console.log(dbs)
+const starCountRef = dbs.ref('control/Henry/2022/7');
+starCountRef.once("value", function (snapshot: any) {
+  console.log("snapshot");
+  console.log(snapshot.val());
+}); */
+export { admin, app, db };
